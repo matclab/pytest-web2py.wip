0b3efd4bcbd955089ea343fd0ac0df1ad9f8dc1f
Mathieu Clabaut <mathieu@clabaut.net>
2016-10-26 11:30:24 +0200

    Allow to output captured stdout and stderr from teradown for failed tests.

--- pytest_web2py/plugin.py
+++ pytest_web2py/plugin.py
@@ -178,3 +178,46 @@ def mute_noisy_tasklets():
     ndb.utils.__tracebackhide__ = True
     ndb.tasklets.__tracebackhide__ = True
     ndb.context.__tracebackhide__ = True
+
+
+def print_teardown_sections(self, rep):
+    """ Monkey patched into TerminalReporter """
+    for secname, content in rep.sections:
+        if 'teardown' in secname:
+            self._tw.sep('-', secname)
+            if content[-1:] == "\n":
+                content = content[:-1]
+            self._tw.line(content)
+
+
+def summary_failures_with_teardonw(self):
+    """ To monkey patch TerminalReporter.summary_failure, in order to display
+    teardown stdout and stderr sections """
+    if self.config.option.tbstyle != "no":
+        reports = self.getreports('failed')
+        if not reports:
+            return
+        self.write_sep("=", "FAILURES")
+        for rep in reports:
+            if self.config.option.tbstyle == "line":
+                line = self._getcrashline(rep)
+                self.write_line(line)
+            else:
+                msg = self._getfailureheadline(rep)
+                markup = {'red': True, 'bold': True}
+                self.write_sep("_", msg, **markup)
+                self._outrep_summary(rep)
+                for report in self.getreports(''):
+                    if report.nodeid == rep.nodeid and report.when == 'teardown':
+                        #self._outrep_summary(report)
+                        self.print_teardown_sections(report)
+
+
+@pytest.hookimpl(tryfirst=True)
+#@pytest.mark.hookwrapper
+def pytest_terminal_summary(terminalreporter, exitstatus):
+    terminalreporter.__class__.print_teardown_sections = print_teardown_sections
+    terminalreporter.__class__.summary_failures = summary_failures_with_teardonw
+    return
+
+
